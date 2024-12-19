017c76fcebe849b6df211e7a134fb02de1a126fc
Wasee Sun <revengersun@gmail.com>
2024-12-20 03:43:21 +0600

    User table created
    Co-authored-by: Azharul Habib <azharulhabib@users.noreply.github.com>

--- backend/e_booking_api/admin.py
+++ backend/e_booking_api/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+from .forms import CustomUserCreationForm
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    """Custom User Admin"""
+    list_display = ('email', 'username')
+    list_filter = ('groups',)
+    prepopulated_fields = {"slug": ("username",)}
+
+    #Fields to be displayed on the user detail page
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password', 'slug')
+        }),
+        ('Personal_info', {
+            'fields': ('name')
+        }),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'groups', 'user_permissions')
+        }),
+        ('Important dates', {
+            'fields': ('last_login',)
+        }),
+    )
+
+    add_form = CustomUserCreationForm
+    # Fields to be displayed in user creation form
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'slug', 'password1', 'password2', 'is_active', 'is_staff')
+        }),
+    )
--- backend/e_booking_api/models.py
+++ backend/e_booking_api/models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+from phonenumber_field.modelfields import PhoneNumberField
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+import re
+
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    """Custom User Manager"""
+    
+    def create_user(self, email, password, **extra_fields):
+        '''Create and save a new user'''
+        
+        if not email:
+            raise ValueError('User must have an email address')
+        if not password:
+            raise ValueError('User must have a password')
+        
+        try:
+            validate_email(email)
+        except:
+            raise ValidationError('User must have a valid email address')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, password, extra_fields):
+        '''Create and save a new superuser'''
+        
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+        
+        return self.create_user(email, password, **extra_fields)
+    
+
+class User(AbstractBaseUser, PermissionsMixin):
+    '''Custom User Model'''
+    
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+    
+    def __str__(self):
+        return self.email
+    
+class Admin(User):
+    is_staff = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.email
+    
+class Customer(User):
+    phone_number = PhoneNumberField(unique=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    
+    def __str__(self):
+        return self.email
+    
+class Owner(User):
+    phone_number = PhoneNumberField(unique=True)
+    address = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.email
+    
